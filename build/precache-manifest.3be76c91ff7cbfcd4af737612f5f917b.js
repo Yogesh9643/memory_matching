0767e5fe9b40f315b52a35e5f61f7f7ecf180a83
Yogesh9643 <rajputyogesh563@gmail.com>
2020-08-19 12:47:31 +0530

    finalising things

---
+++ build/precache-manifest.3be76c91ff7cbfcd4af737612f5f917b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9534e0142085a0bfb7fe70b1c805a3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f866f865479882566b7",
+    "url": "/static/css/main.2dfabd76.chunk.css"
+  },
+  {
+    "revision": "9894e86001343bf4f9d9",
+    "url": "/static/js/2.cf040356.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf040356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f866f865479882566b7",
+    "url": "/static/js/main.fa236910.chunk.js"
+  },
+  {
+    "revision": "81ea5f0b8e6ab1c36bb7",
+    "url": "/static/js/runtime-main.11503c19.js"
+  }
+]);
