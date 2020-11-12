9f844948cd69495cdee300f50bfec2ca59aa205b
Yogesh9643 <rajputyogesh563@gmail.com>
2020-11-12 22:45:41 +0530

    Updates

---
+++ precache-manifest.9417c3da1ac87f9e7c005257b05904ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf49dff1d105025689d79bd7345c7fc9",
+    "url": "/memory_matching/index.html"
+  },
+  {
+    "revision": "ca578fddb973d22558c4",
+    "url": "/memory_matching/static/css/main.2dfabd76.chunk.css"
+  },
+  {
+    "revision": "bdb81dc24ddac21f51cd",
+    "url": "/memory_matching/static/js/2.de96cd10.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/memory_matching/static/js/2.de96cd10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca578fddb973d22558c4",
+    "url": "/memory_matching/static/js/main.b1b164b7.chunk.js"
+  },
+  {
+    "revision": "c8d3119179eb0880fa82",
+    "url": "/memory_matching/static/js/runtime-main.480c4dac.js"
+  }
+]);
